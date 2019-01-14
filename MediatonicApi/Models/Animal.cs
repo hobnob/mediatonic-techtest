efb2a0560b5d7b3f9b811ed332d85166b66e98ca
David North <david.north@purplekingdomgames.com>
2019-01-14 12:19:16 +0000

    Adds animal feed/stroke logic and happiness/hunger calcs

--- MediatonicApi/Models/Animal.cs
+++ MediatonicApi/Models/Animal.cs
@@ -7,5 +7,9 @@ namespace MediatonicApi.Models
         public string TypeName { get; set; }
         public uint HungerPerSecond { get; set; }
         public uint SadnessPerSecond { get; set; }
+
+        public uint MaxHunger { get; set; }
+        public int MinHappiness { get; set; }
+        public int MaxHappiness { get; set; }
     }
 }
--- MediatonicApi/Models/UserAnimal.cs
+++ MediatonicApi/Models/UserAnimal.cs
@@ -4,15 +4,44 @@ namespace MediatonicApi.Models
 {
     public class UserAnimal
     {
-        public uint UserId { get; set; }
-        public uint AnimalId { get; set; }
+        public User User { get; set; }
+        public Animal Animal { get; set; }
         public string AnimalName { get; set; }
         public DateTime LastHungerUpdate { get; set; }
         public DateTime LastHappinessUpdate { get; set; }
         public uint HungerAtUpdate { get; set; }
         public int HappinessAtUpdate { get; set; }
 
-        public int Happiness { get; }
-        public uint Hunger { get; }
+        public double Happiness {
+            get
+            {
+                return Math.Max(
+                    Animal.MaxHappiness,
+                    HappinessAtUpdate - (Animal.SadnessPerSecond * (DateTime.UtcNow - LastHappinessUpdate).TotalSeconds)
+                );
+            }
+        }
+
+        public double Hunger {
+            get
+            {
+                return Math.Min(
+                    Animal.MaxHunger,
+                    HungerAtUpdate + (Animal.HungerPerSecond * (DateTime.UtcNow - LastHungerUpdate).TotalSeconds)
+                );
+            }
+        }
+
+        public void Feed(uint foodAmount)
+        {
+            HungerAtUpdate = (uint) Math.Max((int) Hunger - (int) foodAmount, 0);
+            LastHungerUpdate = DateTime.UtcNow;
+        }
+
+        public void Stroke(uint happinessAmount)
+        {
+            HappinessAtUpdate = Math.Min(HappinessAtUpdate + (int)happinessAmount, Animal.MaxHappiness);
+            LastHappinessUpdate = DateTime.UtcNow;
+        }
     }
 }
