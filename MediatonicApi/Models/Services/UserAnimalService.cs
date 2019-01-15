cacade7407e66c05e6e3a23bd998f173a5bcb5b8
David North <david.north@purplekingdomgames.com>
2019-01-15 12:56:31 +0000

    Fills out the UserAnimalService with new methods - ready to test

--- MediatonicApi/Models/Services/UserAnimalService.cs
+++ MediatonicApi/Models/Services/UserAnimalService.cs
@@ -1,11 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using MediatonicApi.Models.Exceptions;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace MediatonicApi.Models.Services
 {
     public class UserAnimalService
     {
+        private ApiContext _context;
+
+        public UserAnimalService(ApiContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(UserAnimal userAnimal)
+        {
+            // Get all user information
+            User user = _context
+                .Users
+                .Include(u => u.Animals)
+                .FirstOrDefault(u => u.Id == userAnimal.UserId);
+
+            if (user == null) {
+                throw new NotFoundException("User ID does not exist");
+            }
+
+            Animal animal = _context.Animals.FirstOrDefault(a => a.Id == userAnimal.AnimalId);
+            if (animal == null) {
+                throw new NotFoundException("Animal ID does not exist");
+            }
+
+            if (user.Animals.Any(a => a.AnimalId == userAnimal.AnimalId)) {
+                throw new DuplicateEntryException("User already owns '" + animal.TypeName + "'");
+            }
+
+            _context.Add(userAnimal);
+            _context.SaveChanges();
+        }
+
+        public void Update(UserAnimal userAnimal)
+        {
+            if (!_context.UserAnimals.Any(ua => ua.AnimalId == userAnimal.AnimalId && ua.UserId == userAnimal.UserId)) {
+                throw new NotFoundException("User/Animal combination does not exist");
+            }
+
+            _context.Update(userAnimal);
+            _context.SaveChanges();
+        }
+
+        public UserAnimal Get(uint userId, uint animalId)
+        {
+            return _context.UserAnimals.FirstOrDefault(ua => ua.UserId == userId && ua.AnimalId == animalId);
+        }
     }
 }
