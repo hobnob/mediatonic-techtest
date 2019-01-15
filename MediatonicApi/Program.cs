ea128a1e13bfdca12fe28d24134cb3fc4132f435
David North <david.north@purplekingdomgames.com>
2019-01-15 15:33:16 +0000

    Adds DB connection to the API

--- MediatonicApi/Program.cs
+++ MediatonicApi/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using MediatonicApi.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace MediatonicApi
@@ -14,7 +11,21 @@ namespace MediatonicApi
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            IWebHost host = CreateWebHostBuilder(args).Build();
+
+            using (IServiceScope scope = host.Services.CreateScope()) {
+                IServiceProvider services = scope.ServiceProvider;
+
+                try {
+                    // Ensure that the database is craeted before running our API
+                    services.GetRequiredService<ApiContext>().Database.EnsureCreated();
+                } catch (Exception ex) {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
