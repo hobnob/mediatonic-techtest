9acc58d5a1297bc0030e9dad43d26bc32c7bfca5
David North <david.north@purplekingdomgames.com>
2019-01-15 13:02:10 +0000

    Adds Foreign key contraints and unique indexes to the database context

--- MediatonicApi/Models/ApiContext.cs
+++ MediatonicApi/Models/ApiContext.cs
@@ -20,9 +20,15 @@ namespace MediatonicApi.Models
             modelBuilder.Entity<UserAnimal>().Property("lastHappinessUpdate");
             modelBuilder.Entity<UserAnimal>().Property("hungerAtUpdate");
             modelBuilder.Entity<UserAnimal>().Property("happinessAtUpdate");
+            modelBuilder.Entity<UserAnimal>().HasOne(ua => ua.User);
+            modelBuilder.Entity<UserAnimal>().HasOne(ua => ua.Animal);
 
+            modelBuilder.Entity<User>().HasIndex(u => u.DisplayName).IsUnique();
             modelBuilder.Entity<User>().Property(u => u.Id).ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<Animal>().HasIndex(a => a.TypeName).IsUnique();
             modelBuilder.Entity<Animal>().Property(a => a.Id).ValueGeneratedOnAdd();
+
         }
     }
 }
